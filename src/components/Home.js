5cfdc1c781bd887c3c5c19109f905ecefec5374d
bhumixa <bhumi.patel2512@gmail.com>
2022-11-03 20:33:16 +0530

    news with map functions and dynamic data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,31 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 //import PropTypes from 'prop-types'
+import newsData from '../news.json';
 
 export class Home extends Component {
-    //   static propTypes = {
-
-    //   }
+    articles = newsData
+    
+    constructor() {
+        super()
+        this.state = {
+            articles: this.articles,
+            loading: false
+        }
+    }
 
     render() {
         return (
             <div className='container my-3'>
                 <h2>Top Headlines</h2>
+
                 <div className="row">
-                    <div className="col md-3">
-                        <NewsItem title="title" description="description" />
-                    </div>
-                    <div className="col md-3">
-                        <NewsItem title="title" description="description" />
-                    </div>
-                    <div className="col md-3">
-                        <NewsItem title="title" description="description" />
-                    </div>                   
+                    {this.articles.map((article) => {
+                        return <div className="col md-3" key={article.url}>
+                            <NewsItem  title={article.title} description={article.description} imgUrl={article.urlToImage} newsUrl={article.url} />
+                        </div>
+                    })}
+
                 </div>
 
             </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,23 +2,26 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 export class NewsItem extends Component {
+   
     static propTypes = {
 
     }
 
     render() {
-        let { title, description } = this.props;
+        let { title, description, imgUrl, newsUrl } = this.props;
 
         return (
             <>
-                <div className="card" style={{width: '18rem'}}>
-                    <img src="..." className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <h5 className="card-title">
-                            {title}
-                        </h5>
-                        <p className="card-text"> {description} </p>
-                        <a href="/" className="btn btn-primary">Show</a>
+                <div className="my-3">
+                    <div className="card" style={{ width: '18rem' }}>
+                        <img src={imgUrl ? imgUrl : process.env.PUBLIC_URL + '/no-image.jpeg'} className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">
+                                {title}
+                            </h5>
+                            <p className="card-text"> {description} </p>                            
+                            <a href={newsUrl} className="btn btn-sm btn-primary">Read More</a>
+                        </div>
                     </div>
                 </div>
             </>
