1cbde0985e62f865e1550763b11571dfe1ed00b4
bhumixa <bhumi.patel2512@gmail.com>
2022-11-03 21:08:33 +0530

    news with map functions and dynamic data

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,7 @@ export class Home extends Component {
     articles = newsData
 
     constructor() {
+        console.log('constructor')
         super()
         this.state = {
             articles: this.articles,
@@ -14,14 +15,24 @@ export class Home extends Component {
         }
     }
 
+    async componentDidMount(){
+        console.log('componentDidMount')
+        let dataUrl = 'https://newsapi.org/v2/top-headlines?apiKey=ec0477e5e4154ba3a90b3fbb5bbf1a65&country=us';
+        let d = await fetch(dataUrl)
+        let n = await d.json()
+        console.log(n)
+        this.setState({articles:n.articles})
+
+    }
+
     render() {
+        console.log('render')
         return (
             <div className='container my-3'>
                 <h2>Top Headlines</h2>
-
                 <div className="row">
                     {this.articles.map((article) => {
-                        return <div className="col md-3" key={article.url}>
+                        return <div className="col-md-3" key={article.url}>
                             <NewsItem title={article.title ? article.title.slice(0, 45) : ''} description={article.description ? article.description.slice(0, 80) : ""} imgUrl={article.urlToImage} newsUrl={article.url} />
                         </div>
                     })}
